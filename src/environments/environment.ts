52d0af76d56e41274dd69f3cf8b6cb4774a3066a
Aman Dwivedi <77925427+AmanDwivedi01@users.noreply.github.com>
2025-04-23 16:07:27 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'  // Update this to your backend URL
+  apiUrl: 'https://myblog-lgth.onrender.com/api'  // Update this to your backend URL
 };
